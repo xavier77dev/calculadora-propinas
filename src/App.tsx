78badb00811f0778ab7abdbaec517111e3f1854a
xavier77dev <xavier.basurto.77@gmail.com>
2024-08-21 16:36:37 -0500

    feat: create header

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ function App() {
 
   return (
     <>
-      <header>
-        <h1>Calculadora de Propinas y Consumo</h1>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
       </header>
     </>
   )
